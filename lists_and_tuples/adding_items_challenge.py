21a2ad2278a3aab6ff62a7819dbb3d6e1b977fc1
chirag-gurumurthy <chirag.gurumurthy@outlook.com>
2021-07-25 20:29:30 +0200

    adding adding_items_challenge code

---
+++ lists_and_tuples/adding_items_challenge.py
@@ -0,0 +1,31 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+# write your code here
+for i in range(len(data)):
+    if "Flower" in data[i]:
+        flowers.append(data[i])
+    elif "Shrub" in data[i]:
+        shrubs.append(data[i])
